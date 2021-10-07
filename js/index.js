cdb342f7bf114a3dd12c100b02d1f35cc62b02d7
emi4218 <emi4218@gmail.com>
2021-10-07 09:17:19 +0200

    07/10/21 des trucs à revoir mais ça fonctionne

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 window.addEventListener('load', () => {
 
+    let images = document.querySelectorAll(".min")
+    let gdeImage = document.querySelector(".imagePrincipale")
 
+    // images.forEach(image => {
+    //     image.addEventListener('click', event => {
+    //         gdeImage.
+
+    //     })
+    // });
+
+    let minis = document.querySelectorAll('.min')
+    let imgGrand = document.querySelector('.imagePrincipale img')
+
+    for (let i = 0; i < minis.length; i++) {
+        minis[i].addEventListener('click', (e) => {
+            imgGrand.src = e.target.src
+        })
+    }
 
 });
